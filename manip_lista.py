574cd20d9dfa4e622d99debc0834506f4bd99692
LuidyF <luidyfeitosa1993@outlook.com>
2026-04-09 21:42:13 -0300

    Manipulação de lista em Python

---
+++ manip_lista.py
@@ -0,0 +1,20 @@
+#Entrada inicial valores lista
+lista = [10, 20, 30, 40, 50]
+
+#mudança item posição 1
+lista[1] = 200
+
+#adição de valor ao final da lista
+lista.append(600)
+
+#inserção de item na lista
+lista.insert(2, 300)
+print(lista)
+
+#apagar ultimo item da lista
+lista.pop()
+print(lista)
+
+#apagar primeiro item da lista
+lista.pop(0)
+print(lista)
