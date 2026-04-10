2cd29f9bda8b67d3486bfaf68616d3baa71637fc
LuidyF <luidyfeitosa1993@outlook.com>
2026-04-09 21:48:46 -0300

    Tabuada em Python

--- multi_table.py
+++ multi_table.py
@@ -1,7 +1,9 @@
+#Entrada do valor utilizado no calculo
 tabuada = int(input("Insira um número: "))
 
-
 print(f"Tabuada do {tabuada}")
+
+#Estrutura do calculo da tabuada
 for multiplicador in range(1, 11):
     resultado = tabuada * multiplicador
     print(f"{tabuada} x {multiplicador} = {resultado}")
